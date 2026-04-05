7f405e58a1bb84a4cf1b6796dc9336c7d9ce19fc
Sourabh Raghuwanshi <sraghuwa752@gmail.com>
2026-04-05 16:01:17 +0530

    changes after deployment

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100                   // max 100 requests per IP
+    max: 1000                   // max 100 requests per IP
 });
 
 app.use(limiter);
